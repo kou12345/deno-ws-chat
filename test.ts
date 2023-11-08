5ff8f30a771d4927463f74105d6cc11274f5a4d9
kou12345 <hi.0412.ko.2002@gmail.com>
2023-11-09 00:45:14 +0900

    db setup

--- test.ts
+++ test.ts
@@ -1,3 +1,18 @@
+// @deno-types="https://esm.sh/@planetscale/database/dist/index.d.ts"
+import "https://deno.land/std/dotenv/load.ts";
+import { connect } from "npm:@planetscale/database";
+
+const config = {
+  host: Deno.env.get("DATABASE_HOST"),
+  username: Deno.env.get("DATABASE_USERNAME"),
+  password: Deno.env.get("DATABASE_PASSWORD"),
+};
+
+const db = connect(config);
+
+const result = await db.execute("SELECT * FROM messages");
+console.log(result);
+
 // server.ts
 const port = 8080;
 const conn = Deno.listen({ port });
